b5834cdd28fb88f0d55d601233be9512d6537ddb
cskmacedo <kleber.barreto@msn.com>
2022-05-15 02:33:59 -0300

    Classes Repository e Service corrigidas

--- src/main/kotlin/br/com/lambdateam/myaccess/repository/UserRepository.kt
+++ src/main/kotlin/br/com/lambdateam/myaccess/repository/UserRepository.kt
@@ -1,13 +1,10 @@
 package br.com.lambdateam.myaccess.repository;
 
 import br.com.lambdateam.myaccess.model.UserModel
+import org.apache.catalina.realm.X509UsernameRetriever
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<UserModel, Long> {
 
-    fun findByUsername(username: UserModel)
-
-    fun existsByUsername(username: UserModel): BooleanArray {
-        return booleanArrayOf()
-    }
+    fun findByUsernameContaining(username: String): List<UserModel>
 }
--- src/main/kotlin/br/com/lambdateam/myaccess/service/UserService.kt
+++ src/main/kotlin/br/com/lambdateam/myaccess/service/UserService.kt
@@ -1,12 +1,24 @@
 package br.com.lambdateam.myaccess.service
 
 import br.com.lambdateam.myaccess.model.UserModel
+import br.com.lambdateam.myaccess.repository.UserRepository
+import org.springframework.stereotype.Service
 
-open class UserService {
+@Service
+class UserService (
+    val userRepository: UserRepository
+        ) {
 
-    fun authUser(username: UserModel, password: UserModel) {}
-
-    fun saveUser(username: UserModel, password: UserModel) {}
-
-    fun userValidation(username: UserModel): UserModel { return username}
+    fun getAll(username: String?): List<UserModel> {
+        username?.let {
+            return userRepository.findByUsernameContaining(it)
+        }
+        return userRepository.findAll().toList()
+    }
 }
+
+//    fun authUser(username: UserModel, password: UserModel) {}
+//
+//    fun saveUser(username: UserModel, password: UserModel) {}
+//
+//    fun userValidation(username: UserModel): UserModel { return username}
